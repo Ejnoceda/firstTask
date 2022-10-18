b7d40a5864a41cfc78988ce193fd402677cd4df0
Ejnoceda <109960765+Ejnoceda@users.noreply.github.com>
2022-10-18 23:15:31 +0100

    second failed test

--- src/test/java/firstTaskTest.java
+++ src/test/java/firstTaskTest.java
@@ -9,4 +9,9 @@ public class firstTaskTest {
     public void minimumValue(){
         assertEquals(-2 , StatisticsGiver.calculateStats(new int[]{6, 9, 15, -2, 92, 11}));
     }
+
+    @Test
+    public void maxValue(){
+        assertEquals(92 , StatisticsGiver.calculateStats(new int[]{6, 9, 15, -2, 92, 11}));
+    }
 }
