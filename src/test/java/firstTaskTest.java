64d33a1c461f8c9f4afe716f314cad8a404610d6
Ejnoceda <109960765+Ejnoceda@users.noreply.github.com>
2022-10-18 23:08:46 +0100

    first failed test

---
+++ src/test/java/firstTaskTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class firstTaskTest {
+
+    StatisticsGiver statisticsGiver = new StatisticsGiver();
+    @Test
+    public void minimumValue(){
+        assertEquals(-2 , StatisticsGiver.calculateStats(new int[]{6, 9, 15, -2, 92, 11}));
+    }
+}
